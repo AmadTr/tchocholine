84999b594d683793ef5308e0f7cb1775f448af5d
Pagnest <ultima27@hotmail.com>
2022-04-15 15:51:38 +0200

    ajout trie par catégorie dropdown dans bar de nav

--- src/Controller/TchoChoLineController.php
+++ src/Controller/TchoChoLineController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +17,7 @@ class TchoChoLineController extends AbstractController
      */
     public function index(
         Request $request,
-        ProductRepository $productRepository
+        ProductRepository $productRepository,CategoryRepository $categoryRepository
     ): Response {
         
        
@@ -27,6 +29,20 @@ class TchoChoLineController extends AbstractController
         $products = $productRepository->findAll();
         return $this->render('tcho_cho_line/index.html.twig', [
             'products' => $products,
+            'categories' => $categoryRepository->findAll()
+
         ]);
     }
+    /**
+ * @Route("/{category}", name="app_find_category", methods={"GET"})
+ */
+public function findByCategory(Category $category,ProductRepository $productRepository,CategoryRepository $categoryRepository): Response
+{
+    
+    
+    return $this->render('tcho_cho_line/index.html.twig', [
+        'products' => $productRepository->findBy(['category'=> $category]),
+        'categories' => $categoryRepository->findAll()
+    ]);
+}
 }
